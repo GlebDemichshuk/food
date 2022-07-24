a030e4f8356f00892e1705f8f5757b9337fc01a0
Gleb Demichshuk <cldfear457@gmail.com>
2022-07-24 22:54:26 +0600

    feat: add tag bar

--- frontend/src/app/sevices/food.service.ts
+++ frontend/src/app/sevices/food.service.ts
@@ -1,19 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Food} from "../shared/models/Food";
-import { data_foods } from 'src/data';
+import {data_foods, data_tags} from 'src/data';
+import {Tag} from "../shared/models/Tag";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FoodService {
 
-  constructor() { }
+  constructor() {
+  }
 
   getAll(): Food[] {
     return data_foods;
   }
 
-  getAllFoodsBySearchTerm(searchTerm: string){
+  getAllFoodsBySearchTerm(searchTerm: string) {
     return this.getAll().filter(food => food.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  }
+
+  getAllTags(): Tag[] {
+    return data_tags;
+  }
+
+  getAllFoodsByTag(tag: string): Food[] {
+    return tag == "All" ?
+      this.getAll() :
+      this.getAll().filter(food => food.tags?.includes(tag));
   }
 }
